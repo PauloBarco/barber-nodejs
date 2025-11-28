2e521154b35c003eb89284479d9bfc856871b93c
PauloBarco <paulo.barco34@gmail.com>
2025-11-28 14:39:17 -0300

    Barber  V1.3

--- modules/fs.js
+++ modules/fs.js
@@ -1,11 +1,50 @@
+const { ifError } = require("assert");
+const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 
-//Criar uma pasta
+// Criar uma pasta
 // fs.mkdir(path.join(__dirname, "/test"), (error) => {
 //   if (error) {
 //     return console.log("Erro: ", error);
 //   }
 
 //   console.log("Pasta criada com sucesso");
-// });
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ,erro");
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  }
+);
+
+// Adicionar a um arquivo
+fs.appendFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello world!",
+  (error) => {
+    if (error) {
+      return console.log("Error: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  }
+);
+
+// Ler Arquivo
+fs.readFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "utf8",
+  (error, data) => {
+    if (error) {
+      return console.log("Erro ", error);
+    }
+    console.log(data);
+  }
+);
