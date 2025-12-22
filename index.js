2776d7aef9ef54f5553e473283f22c4a8120bf30
PauloBarco <paulo.barco34@gmail.com>
2025-12-22 14:01:14 -0300

    MongoDB V 3

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      "https://cloud.mongodb.com/v2/694970108005a44268f7e92b#/overview?connectCluster=NodeJs"
+    );
+    console.log("Conectado ao MongoDB!");
+  } catch (error) {
+    console.error("Erro ao conectar ao MongoDB:", error);
+  }
+};
+
+module.exports = connectToDatabase;
+
 // const { Person } = require("./person");
-const dotenv = require("dotenv");
-const connectToDatabase = require("./src/databse/connect");
+// const dotenv = require("dotenv");
+// const connectToDatabase = require("./src/databse/connect");
 
-dotenv.config();
+// dotenv.config();
 
-connectToDatabase();
+// connectToDatabase();
 
 // require("./modules/path");
 // require("./modules/fs");
