8700979e4092dd9d0e1ac52edad2a84c9be0705f
PauloBarco <paulo.barco34@gmail.com>
2025-12-22 12:56:49 -0300

    V3.1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Person } = require("./person");
 
 // require("./modules/path");
-//require("./modules/fs");
-require("./modules/http.js");
+// require("./modules/fs");
+// require("./modules/http.js");
+
+require("./modules/express");
 
 const person = new Person("Paulo");
