a289d3f8bc7cdd4aa2955f73649c5cee45b1cdc7
PauloBarco <paulo.barco34@gmail.com>
2025-12-23 15:50:23 -0300

    Banco V2

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const UserModel = require(`../src/models/users.model`);
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/home", (req, res) => {
   res.contentType("application/html");
@@ -22,10 +25,14 @@ app.get("/users", (req, res) => {
   res.status(200).json(users);
 });
 
-app.post("/users", (req, res) => {
-  const user = UserModel.create(req.body);
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
 
-  res.status(201).json(user);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
