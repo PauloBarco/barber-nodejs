b374df5fe7994fd85dfe596a9121f26fc16ce373
PauloBarco <paulo.barco34@gmail.com>
2025-12-23 15:05:37 -0300

    correção

--- modules/express.js
+++ modules/express.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const UserModel = require(`../src/models/user.model`);
+const UserModel = require(`../src/models/user.models`);
 const app = express();
 
 app.get("/home", (req, res) => {
